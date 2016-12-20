6aba00357ba87cfcf04ac1460d9642b5f70845b8
Rishi Raj <rishiraj824@gmail.com>
2016-12-20 09:11:42 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+  $("#partners").owlCarousel({
+ 
+      autoPlay: false, //Set AutoPlay to 3 seconds
+ 
+      items : 5,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+  $("#blog").owlCarousel({
+ 
+      autoPlay: false, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
